1ab0e1b5621c3e5fd8ea1953ef27aa76dfc86e4c
Brian Duong <brianminhduong@gmail.com>
2022-05-24 21:58:02 -0400

    Set-up skeleton for Backend

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const compression = require("compression");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+//Server set-up
+const PORT = process.env.PORT || 8080;
+const app = express();
+const dev = app.get("env") !== "production";
+
+//If not in development
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(compression());
+  app.use(morgan("common"));
+
+  app.use(express.static(path.resolve(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+if (dev) {
+  app.use(morgan("dev"));
+}
+
+//Start Server
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`Listening on port: ${PORT}`);
+});
--- src/server.js
+++
@@ -1,3 +0,0 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
