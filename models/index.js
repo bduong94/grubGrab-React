d430c993e4c271ab9eca92a37fc458dbcb0edf87
Brian Duong <brianminhduong@gmail.com>
2022-05-29 21:53:32 -0400

    Initialize Sequelize Models setup

---
+++ models/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const {
+  host,
+  user,
+  password,
+  database,
+  dialect,
+} = require("../config/db.config");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(database, user, password, {
+  host: host,
+  dialect: dialect,
+  operatorsAliases: false,
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
