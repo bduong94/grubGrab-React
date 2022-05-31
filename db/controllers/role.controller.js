06ee67f1564629c14cd54d5dc04157cbff28403a
Brian Duong <brianminhduong@gmail.com>
2022-05-31 19:32:13 -0400

    Rename Role table to Roles

--- db/controllers/role.controller.js
+++ db/controllers/role.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const Role = db.roles;
+const Role = db.Roles;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Test
--- db/models/role.js
+++ db/models/role.js
@@ -1,9 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
+  class Roles extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +11,14 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Role.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Role',
-  });
-  return Role;
+  Roles.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Roles",
+    }
+  );
+  return Roles;
 };
