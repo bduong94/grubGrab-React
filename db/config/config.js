7593259c525133b89a540d43255b9da26d57bd2f
Brian Duong <brianminhduong@gmail.com>
2022-05-30 21:33:07 -0400

    Switch over DB to use migrations

---
+++ db/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: "postgresql",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: "postgresql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: "postgresql",
+  },
+};
--- db/models/role.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-  const Role = sequelize.define("role", {
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-  });
-  return Role;
-};
