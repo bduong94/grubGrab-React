4a90276d7ec3571bdd18de7dec114a70c0744b66
Brian Duong <brianminhduong@gmail.com>
2022-05-31 21:38:20 -0400

    Refactor Roles table

--- db/seeders/20220531233532-roles.js
+++ db/seeders/20220531233532-roles.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert("Roles", [
+    return queryInterface.bulkInsert("roles", [
       {
         name: "Customer",
         created_at: new Date(),
