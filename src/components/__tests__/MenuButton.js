5fcafe9fe071d5022930387dd9307a99fd7b367c
Brian Duong <brianminhduong@gmail.com>
2022-06-02 21:53:57 -0400

    Add additional tests for menuItem buttons

--- src/components/__tests__/MenuButton.js
+++ src/components/__tests__/MenuButton.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import MenuButton from "../MenuButton";
 
 describe("Tests for MenuButton", () => {
   it("Renders without crashing", () => {
     render(<MenuButton />);
+  });
+
+  it("Renders a button with no text", () => {
+    render(<MenuButton />);
+    const addButton = screen.queryByText("+");
+    const minusButton = screen.queryByText("-");
+
+    expect(addButton).toBeNull();
+    expect(minusButton).toBeNull();
+  });
+
+  it("Renders a '+' Button", () => {
+    const { getByText } = render(<MenuButton buttonType="dark" />);
+    expect(getByText("+")).toBeInTheDocument();
+  });
+
+  it("Renders a '-' Button", () => {
+    const { getByText } = render(<MenuButton buttonType="secondary" />);
+    expect(getByText("-")).toBeInTheDocument();
   });
 });
