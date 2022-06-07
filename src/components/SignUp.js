ac4ebf2c6448bea471972150ade39f5b0a206f94
Brian Duong <brianminhduong@gmail.com>
2022-06-06 23:53:41 -0400

    Change class to className in SignUp component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,123 +3,111 @@ import React from "react";
 export default function SignUp() {
   return (
     <>
-      <form class="row g-3 needs-validation" id="signup" novalidate>
-        <div class="col-md-4">
-          <label for="signup-email" class="form-label">
+      <form className="row g-3 needs-validation" id="signup" novalidate>
+        <div className="col-md-4">
+          <label for="signup-email" className="form-label">
             Email
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="signup-email"
-            value="Mark"
             required
           />
-          <div class="valid-feedback">Looks good!</div>
+          <div className="valid-feedback">Looks good!</div>
         </div>
-        <div class="col-md-4">
-          <label for="signup-password" class="form-label">
+        <div className="col-md-4">
+          <label for="signup-password" className="form-label">
             Password
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="signup-password"
-            value="Otto"
             required
           />
-          <div class="valid-feedback">Looks good!</div>
+          <div className="valid-feedback">Looks good!</div>
         </div>
-        <div class="col-md-4">
-          <label for="signup-name" class="form-label">
+        <div className="col-md-4">
+          <label for="signup-name" className="form-label">
             Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="signup-name"
-            value="Otto"
             required
           />
-          <div class="valid-feedback">Looks good!</div>
+          <div className="valid-feedback">Looks good!</div>
         </div>
-        <div class="col-md-4">
-          <label for="signup-surname" class="form-label">
+        <div className="col-md-4">
+          <label for="signup-surname" className="form-label">
             Surname
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="signup-surname"
-            value="Otto"
             required
           />
-          <div class="valid-feedback">Looks good!</div>
+          <div className="valid-feedback">Looks good!</div>
         </div>
-        <div class="col-md-6">
-          <label for="signup-address" class="form-label">
+        <div className="col-md-6">
+          <label for="signup-address" className="form-label">
             Address
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="signup-address"
             required
           />
-          <div class="invalid-feedback">Please provide a valid address.</div>
+          <div className="invalid-feedback">
+            Please provide a valid address.
+          </div>
         </div>
-        <div class="col-md-6">
-          <label for="signup-city" class="form-label">
+        <div className="col-md-6">
+          <label for="signup-city" className="form-label">
             City
           </label>
-          <input type="text" class="form-control" id="signup-city" required />
-          <div class="invalid-feedback">Please provide a valid city.</div>
+          <input
+            type="text"
+            className="form-control"
+            id="signup-city"
+            required
+          />
+          <div className="invalid-feedback">Please provide a valid city.</div>
         </div>
-        <div class="col-md-3">
-          <label for="signup-province" class="form-label">
+        <div className="col-md-3">
+          <label for="signup-province" className="form-label">
             Province
           </label>
-          <select class="form-select" id="signup-province" required>
+          <select className="form-select" id="signup-province" required>
             <option selected disabled value="">
               Choose...
             </option>
             <option>...</option>
           </select>
-          <div class="invalid-feedback">Please select a valid province.</div>
+          <div className="invalid-feedback">
+            Please select a valid province.
+          </div>
         </div>
-        <div class="col-md-3">
-          <label for="signup-postal-code" class="form-label">
+        <div className="col-md-3">
+          <label for="signup-postal-code" className="form-label">
             Postal Code
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="signup-postal-code"
             required
           />
-          <div class="invalid-feedback">
+          <div className="invalid-feedback">
             Please provide a valid postal code.
           </div>
         </div>
-        <div class="col-12">
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="invalidCheck"
-              required
-            />
-            <label class="form-check-label" for="invalidCheck">
-              Agree to terms and conditions
-            </label>
-            <div class="invalid-feedback">
-              You must agree before submitting.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <button class="btn btn-primary" type="submit">
+        <div className="col-12">
+          <button className="btn btn-primary" type="submit">
             Submit form
           </button>
         </div>
